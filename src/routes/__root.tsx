fce6206831411af5ca9c68a7d6443625d350e2e7
Annas Alghoffar <aag.annas@gmail.com>
2025-03-06 00:41:22 +0700

    Initial commit

---
+++ src/routes/__root.tsx
@@ -0,0 +1,22 @@
+import { Outlet, createRootRoute } from "@tanstack/react-router";
+import { TanStackRouterDevtools } from "@tanstack/router-devtools";
+
+import Header from "../components/Header";
+
+import TanstackQueryLayout from "../integrations/tanstack-query/layout";
+
+import TanstackQueryProvider from "../integrations/tanstack-query/provider";
+
+export const Route = createRootRoute({
+	component: () => (
+		<>
+			<TanstackQueryProvider>
+
+				<Outlet />
+				<TanStackRouterDevtools />
+
+				<TanstackQueryLayout />
+			</TanstackQueryProvider>
+		</>
+	),
+});
